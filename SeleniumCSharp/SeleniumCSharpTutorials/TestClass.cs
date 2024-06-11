3e0105f485f54b076509b47cfcca09ae746c67f7
Jakub Korban <jakub.korban@epoka.io>
2024-06-11 18:02:03 +0200

    init

---
+++ SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
@@ -0,0 +1,20 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpTutorials
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            
+        }
+    }
+}

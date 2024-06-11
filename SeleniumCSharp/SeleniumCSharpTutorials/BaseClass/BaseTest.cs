b1e325426575846cb7992696605f7c8fb72af04c
Jakub Korban <jakub.korban@microsoft.wsei.edu.pl>
2024-06-11 21:03:49 +0200

    akceptacja cookies

--- SeleniumCSharp/SeleniumCSharpTutorials/BaseClass/BaseTest.cs
+++ SeleniumCSharp/SeleniumCSharpTutorials/BaseClass/BaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,7 @@ namespace SeleniumCSharpTutorials.BaseClass
     {
         public IWebDriver driver;
 
-        [OneTimeSetUp]
+        [SetUp]
         public void Open()
         {
             ChromeOptions options = new ChromeOptions();
@@ -24,9 +25,21 @@ namespace SeleniumCSharpTutorials.BaseClass
             driver = new ChromeDriver(options);
             driver.Manage().Window.Maximize();
             driver.Url = "https://www.facebook.com/";
+
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div._10.uiLayer._4-hy._3qw > div._59s7._9l2g > div > div > div > div > div:nth-child(3) > div.x1exxf4d.x13fuv20.x178xt8z.x1l90r2v.x1pi30zi.x1swvt13 > div > div:nth-child(2)")));
+                IWebElement acceptCookiesButton = driver.FindElement(By.CssSelector("div._10.uiLayer._4-hy._3qw > div._59s7._9l2g > div > div > div > div > div:nth-child(3) > div.x1exxf4d.x13fuv20.x178xt8z.x1l90r2v.x1pi30zi.x1swvt13 > div > div:nth-child(2)"));
+                acceptCookiesButton.Click();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nie udało się kliknąć przycisku akceptacji plików cookie: " + ex.Message);
+            }
         }
 
-        [OneTimeTearDown]
+        [TearDown]
         public void Close()
         {
             driver.Quit();
--- SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
+++ SeleniumCSharp/SeleniumCSharpTutorials/TestClass.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using SeleniumCSharpTutorials.BaseClass;
 
 namespace SeleniumCSharpTutorials
@@ -11,21 +13,30 @@ namespace SeleniumCSharpTutorials
     [TestFixture]
     public class TestClass : BaseTest
     {
-        [Test]
+        [Test, Category("Smoke Testing")]
         public void TestMethod1()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
+
+            driver.Url = "https://m.facebook.com/reg/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNzE4MTI4MTQwLCJjYWxsc2l0ZV9pZCI6MjY5NTQ4NDUzMDcyMDk1MX0%3D";
+
+            IWebElement monthDropdownList = driver.FindElement(By.XPath(".//*[@id='month']"));
+            SelectElement element = new SelectElement(monthDropdownList);
+
+            element.SelectByIndex(1); // Select by index
+            element.SelectByText("mar"); // Select by text
+            element.SelectByValue("6"); // Select by value
         }
 
-        [Test]
+        [Test, Category("Regression Testing")]
         public void TestMethod2()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
             emailTextField.SendKeys("Selenium C#");
         }
 
-        [Test]
+        [Test, Category("Smoke Testing")]
         public void TestMethod3()
         {
             IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
